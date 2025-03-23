d588375b724eac428fa7ea0c63ab98a737e1d0dc
Adilson de Lima Santos <87918963+Adilson-ls@users.noreply.github.com>
2025-03-23 13:20:09 +0000

    ex2 divisão

---
+++ linguagem C/Operações Matematicas e Variáveis/exercícios de Fixação Operações Matemáticas - EX2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a = 10; //Define valor de a
+    int b = 20; //Define valor de b
+    
+    //Resultado da divisão
+    int resultado = a / b;
+
+    //Exibe resultado na tela
+    printf("Resultado: %d\n", resultado);
+
+    return 0;
+}
