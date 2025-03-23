e4294d1d93866848b68abf24881ab00f3101a254
Adilson de Lima Santos <87918963+Adilson-ls@users.noreply.github.com>
2025-03-23 13:47:51 +0000

    Ex3 Multiplicação numeros quebrados

---
+++ linguagem C/Operações Matematicas e Variáveis/exercícios de Fixação Operações Matemáticas - EX3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float a = 5.7; //Define valor de a
+    float b = 2.0; //Define valor de b
+    
+    //Resultado de multiplicação
+    float resultado = a * b;
+
+    //Exibe resultado na tela
+    printf("Resultado: %f\n", resultado);
+
+    return 0;
+}
