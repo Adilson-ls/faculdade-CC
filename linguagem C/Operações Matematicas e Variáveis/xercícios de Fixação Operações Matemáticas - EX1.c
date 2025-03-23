e298f44e9ff104deb795a98e63b0f37d5f159d25
Adilson de Lima Santos <87918963+Adilson-ls@users.noreply.github.com>
2025-03-23 13:15:44 +0000

    EX1

---
+++ linguagem C/Operações Matematicas e Variáveis/xercícios de Fixação Operações Matemáticas - EX1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a = 10; //Define valor de a
+    int b = 20; //Define valor de b
+    int c = 30; //Define valor de c
+
+    //Resultado da soma
+    int resultado = a + b + c;
+
+    //Exibe resultado na tela
+    printf("Resultado: %d\n", resultado);
+
+    return 0;
+}
